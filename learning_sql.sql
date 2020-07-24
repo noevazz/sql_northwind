08f15e1958efb1878d2f11a3f0a59812bce74295
noe vaz <jonovagu@gmail.com>
2020-07-24 09:42:37 -0500

    [ADD] adding more functions and also starting the translation

--- learning_sql.sql
+++ learning_sql.sql
@@ -1,29 +1,36 @@
--- Seleccionar todas las columnas de una tabla
-SELECT * FROM nombre_de_la_tabla;
+-- .......:::::: Select the whole table ::::::.......
+-- Syntax
+SELECT * FROM table_name;
 
-	-- Seleccionar toda las columnas (osea la tabla) de clientes
+	-- Select all the columns from the customers table
 	SELECT * FROM customers;
 	
-	-- Seleccionar toda las columnas (osea la tabla) de empleados
+	-- Select all the columns from the employees table
 	SELECT * FROM employees;
 
 
 
 
 
--- Seleccionar columnas de una tabla:
-SELECT columna1, columna2, columnaX FROM nombre_de_la_tabla;
-	-- cuando se seleccionan columnas podemos acomodarlas en el orden que queramos
 
-	-- Seleccionar el id, nombre de contacto y ciudad de todos los clientes:
+-- .......:::::: Select specific columns from a table ::::::.......
+SELECT columna1, columna2, columnaX FROM table_name;
+	-- the orders of the columns can be arbitrary
+
+	-- Select the id, contact name and city of all customers
 	SELECT customer_id, contact_name, city FROM customers;
 	
-	-- Seleccionar id, primer nombre, titulo (cargo) y decha de contratación los empleados
+	-- Select id, first name, title and hiring date of all employees
 	SELECT employee_id, first_name, title, hire_date FROM employees;
 
--- Usar alias para las columnas
+
+
+
+
+
+-- .......:::::: Use an alias for each column ::::::.......
 	-- Dependiendo el gestor de bd se debe usar comillas dobles o comillas simples
-SELECT columna1 as nombre, columna2 as "Nombre con espacios" FROM nombre_de_la_tabla;
+SELECT columna1 as nombre, columna2 as "Nombre con espacios" FROM table_name;
 	-- En este caso los alias con espacios  deben estar entre COMILLAS DOBLES
 	-- comillas simples son usadas para definir strings (los aliases usan comillas dobles porque definen aliases, lol)
 
@@ -31,23 +38,47 @@ SELECT columna1 as nombre, columna2 as "Nombre con espacios" FROM nombre_de_la_t
 	SELECT customer_id as identificador, contact_name as "nombre del cliente", city as "ciudad" FROM customers;
 
 -- Seleccionar columas con registros que no esten duplicados
-SELECT DISTINCT columa1, columaX FROM nombre_de_la_tabla;
+SELECT DISTINCT columa1, columaX FROM table_name;
 	-- Seleccionar las ciudades (sin repetir) de la tabla clientes:
 	SELECT DISTINCT city FROM customers;
 
--- Contar registros
-SELECT COUNT(columna) FROM nombre_de_la_tabla;
-	-- Contar cuantos empleados hay
+
+
+
+
+
+-- .......:::::: Use an alias for each column ::::::.......
+-- When using an alias for a table always remember to use the dot notation to select the desired columns:
+-- doted notation aliasUsed.nameOfTheColumn
+	-- Use an alias for the customers table and retrieve the contact name and the title
+	SELECT cus.contact_name, cus.contact_title FROM customers as cus;
+
+
+
+
+
+
+-- .......:::::: Count registries  ::::::.......
+-- The COUNT() function returns the number of rows that matches a specified criterio.
+-- Sintax:
+SELECT COUNT(column) FROM table_name;
+
+	-- Count the number of employees
 	SELECT COUNT(employee_id) FROM employees;
 	
-	-- Contar cuantas ciudades diferentes hay en la tabla clientes
+	-- Count how many diferent cities exists in the customers table
 	SELECT COUNT(DISTINCT city) FROM customers;
 	
-	-- Contar cuantas ciudades diferentes hay en la tabla clientes Y USAR UN ALIAS
-	SELECT COUNT(DISTINCT city) as "numero de ciudaddes" FROM customers;
-		
--- Seleccionar en base a algo
-SELECT columna1, columnaX FROM nombre_de_la_tabla WHERE columnaZ='un valor'
+	-- Count how many diferent cities exists in the customers table AND USE AN ALIAS
+	SELECT COUNT(DISTINCT city) as "AMount of cities" FROM customers;
+
+
+
+
+
+
+-- .......:::::: Select columns bases in a condition (WHERE) ::::::.......
+SELECT columna1, columnaX FROM table_name WHERE columnaZ='un valor'
 	-- Nota que se uso COMILLAS SIMPLES para difinir el string
 
 	-- Seleccionar el nombre de contacto (con el alias "nombre del cliente") y ciudad de los clientes que son de London
@@ -80,12 +111,16 @@ SELECT columna1, columnaX FROM nombre_de_la_tabla WHERE columnaZ='un valor'
 	-- Seleccionar todos los productos que cuesten mas de 15 y menos de 30
 	SELECT product_name, unit_price FROM products WHERE unit_price BETWEEN 15 AND 30;
 	-- BETWEEN x AND Y significa mayor o igual a 15 y menor o igual a 30 (osea se incluyen los dos valores)
+	-- The values can be numbers, text, or dates.
 	
 	-- Seleccionar los clientes que son de London y Berlin
 	SELECT customer_id, contact_name, city FROM customers WHERE city IN ('London', 'Berlin');
 	
 	-- Contar cuantos clientes son de London y Berlin
 	SELECT COUNT(customer_id) FROM customers WHERE city IN ('London', 'Berlin');
+
+	-- Select all the customers that come from the same cities as the employees:
+	SELECT * FROM customers WHERE city IN (SELECT city FROM employees);
 	
 	-- Seleccionar los clientes de un pais que termine en n
 	SELECT customer_id, contact_name, city FROM customers WHERE city LIKE '%n';
@@ -120,8 +155,9 @@ SELECT columna1, columnaX FROM nombre_de_la_tabla WHERE columnaZ='un valor'
 
 
 
--- Ordernar registros de las columnas
-SELECT * FROM nombre_de_la_tabla ORDER BY columnaX ASC|DESC
+
+-- .......:::::: Order registries (ORDER BY) ::::::.......
+SELECT * FROM table_name ORDER BY columnaX ASC|DESC
 	--- ASC|DESC (Ascendente|Descendente) Son valores opcionales, ascendete se usa por defecto si no se especifica nada
 	-- Seleccionar el nombre de contacto (con el alias "nombre del cliente") y ciudad de los clientes que son de London
 	-- y ordenar de manera descendente por la ciudad
@@ -135,10 +171,11 @@ SELECT * FROM nombre_de_la_tabla ORDER BY columnaX ASC|DESC
 
 
 
--- Insertar registros nuevos
-INSERT INTO nombre_de_la_tabla (columna1, columna2, columnaX) VALUES (value1, value2, valueX);
+
+-- .......:::::: Insert new registries ::::::.......
+INSERT INTO table_name (columna1, columna2, columnaX) VALUES (value1, value2, valueX);
 -- Si se van a usar todas las columnas no hay que especificarlas:
-INSERT INTO nombre_de_la_tabla VALUES (value1, value2, valueX);
+INSERT INTO table_name VALUES (value1, value2, valueX);
 	-- Insertar un registro para un cliente, se debe especificar el nombre de la compañia, id y otros valores extras
 	-- esto porque el nombre de la compañia es NOT NULL (requerido) al igual que el id
 	-- por cierto el ID NO tiene AUTO INCREMENT porque es un ocnjunto de caracteres 
@@ -151,7 +188,7 @@ INSERT INTO nombre_de_la_tabla VALUES (value1, value2, valueX);
 
 
 
--- NULL values
+-- .......:::::: About NULL values ::::::.......
 -- Un campo con valor NULL significa que ese campo no tiene valor,
 -- un campo AL QUE NO se le especifico explicitamente el constraint NOT NULL
 -- puede tener NULL como valor (osea puedes no insertarle valor)
@@ -166,8 +203,9 @@ INSERT INTO nombre_de_la_tabla VALUES (value1, value2, valueX);
 
 
 
--- Actualizar registros
-UPDATE nombre_de_la_tabla SET columna1=valor1, columna2=valor2, columna1=valorX WHERE condicion;
+
+-- .......:::::: Update registries (INSERT INTO) ::::::.......
+UPDATE table_name SET columna1=valor1, columna2=valor2, columna1=valorX WHERE condicion;
 -- Si se omite el WHERE se aplicaran los SETs a todos los registros de la tabla
 	-- Averiguar cual es el ID de andressa
 	SELECT customer_id, contact_name from customers where contact_name LIKE 'Andressa%';
@@ -188,8 +226,9 @@ UPDATE nombre_de_la_tabla SET columna1=valor1, columna2=valor2, columna1=valorX 
 
 
 
--- Eliminar registros
-DELETE FROM nombre_de_la_tabla WHERE condicion;
+
+-- .......:::::: Delete registries (DELETE FROM) ::::::.......
+DELETE FROM table_name WHERE condicion;
 	-- Eliminar el registro de Andressa
 	DELETE FROm customers WHERE customer_id='ANDRE';
 
@@ -199,9 +238,10 @@ DELETE FROM nombre_de_la_tabla WHERE condicion;
 
 
 
+
 	
--- Especificar la cantidad de registros a regresar
--- Dependiendo el sistema de bd se puede usar:
+-- .......:::::: Limit the amout of registries to return ::::::.......
+-- Depending of the DB management system we can use:
 
 -- SQL Server/MS Access:
 SELECT TOP number|percent column_name(s) FROM table_name WHERE condition; 
@@ -212,5 +252,49 @@ SELECT column_name(s) FROM table_name WHERE condition LIMIT number;
 -- Oracle Syntax:
 SELECT column_name(s) FROM table_name WHERE ROWNUM <= number; 
 
--- La condicion es opcional solo se uso para mostrar como se pondria
+-- The WHERE is optional but was used to show how we can use it in conjuntion with LIMIT
 
+	-- Select the last 2 registries from the customers table that are from Colima (it's a city)
+	SELECT * FROM customers WHERE city='Colima' ORDER BY customer_id DESC LIMIT 3;
+	-- By default Limit returns the registries on the top of the specified table,
+	-- that's why I used ORDER BY (to put the last items on the top)
+
+
+
+
+
+
+-- .......:::::: SQL MIN() and MAX() Functions ::::::.......
+-- The MIN() function returns the smallest value of the selected column.
+-- SINTAX of MIN(): 
+SELECT MIN(column_name) FROM table_name WHERE condition; 
+	-- Select the cheapest unit price (unit_price) in the products table
+	SELECT MIN(unit_price) AS SmallestPrice FROM products;
+	
+	-- Select the name of the cheapest product
+	SELECT product_name, unit_price FROM products WHERE unit_price=(SELECT MAX(unit_price) as "Cheapest Product" FROM products)
+
+-- The MAX() function returns the largest value of the selected column.
+	-- Select the most expensive unit price (unit_price) in the products table
+	SELECT MAX(unit_price) AS "MOST expensive Product" FROM products;
+	
+	-- Select the name of the most expensive product
+	SELECT product_name, unit_price FROM products WHERE unit_price=(SELECT MAX(unit_price) as "Cheapest Product" FROM products)
+
+
+
+
+
+
+-- .......:::::: SQL AVG() and SUM() Functions ::::::.......
+-- The AVG() function returns the average value of a numeric column.
+-- Syntax
+SELECT AVG(column_name) FROM table_name WHERE condition; 
+	-- Select the average price of all products
+	SELECT AVG(unit_price) FROM products;
+
+-- The SUM() function returns the total sum of a numeric column.
+-- Syntax
+SELECT SUM(column_name) FROM table_name WHERE condition; 
+	-- Find the average price of all products that costs more than 15 (unit_price)
+	SELECT SUM(unit_price) FROM products WHERE unit_price>15;
